096fa105b6d48b79769232cec970f4ca5fc797c8
Declyn50s <derval.botuna@gmail.com>
2025-11-27 23:53:32 +0100

    Fix router basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,8 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      {/* IMPORTANT : basename pour GitHub Pages */}
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
