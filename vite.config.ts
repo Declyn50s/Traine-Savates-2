564387591e6398d0b63333bc9cef8b1cfa370e33
Declyn50s <derval.botuna@gmail.com>
2025-11-27 23:42:07 +0100

    Configure build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Traine-Savates-2/",
-})
+  build: {
+    outDir: "docs",   // <— important pour GitHub Pages
+  },
+});
